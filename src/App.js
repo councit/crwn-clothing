02881d580b0560cd1332436a55d022bb60d34649
Taylor Council <councit@gmail.com>
2020-03-30 15:26:23 -0700

    pushing routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
-      <h1>this is a test</h1>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
