b2b375001abac2d5cf137123dc368aa22cde6f76
Taylor Council <councit@gmail.com>
2020-03-31 16:24:27 -0700

    created sign in form with custom button component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ email: '', password: '' });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            value={this.state.email}
+            label='email'
+            handleChange={this.handleChange}
+            required
+          />
+
+          <FormInput
+            name='password'
+            type='password'
+            label='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            required
+          />
+
+          <CustomButton type='submit' value='Submit Form'>
+            Sign In
+          </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
