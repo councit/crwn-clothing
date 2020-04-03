35fc804460be0bff86f5c8b09218df2fc2651c04
Taylor Council <councit@gmail.com>
2020-04-02 18:17:01 -0700

    created sign in component and sign in with google auth, added styling

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -53,13 +53,12 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
             required
           />
-
-          <CustomButton type='submit' value='Submit Form'>
-            Sign In
-          </CustomButton>
-          <CustomButton onClick={signInWithGoogle} value='Submit Form'>
-            Sign In with Google
-          </CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'>Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
